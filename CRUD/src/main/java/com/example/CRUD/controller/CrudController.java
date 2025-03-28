9629352039e6ff87a6b7cdeae01b0a2092059b4d
Gaurb <89831065+Gaurb@users.noreply.github.com>
2025-03-28 18:19:11 +0530

    initial commit

---
+++ CRUD/src/main/java/com/example/CRUD/controller/CrudController.java
@@ -0,0 +1,47 @@
+package com.example.CRUD.controller;
+
+import com.example.CRUD.model.Student;
+import com.example.CRUD.service.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CrudController {
+    @Autowired
+    private CrudService crudService;
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello";
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudent(){
+        return crudService.getAllStudent();
+    }
+    @PostMapping("/create")
+    public Student createStudent(@RequestBody Student student){
+        return crudService.createStudent(student);
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable int id){
+        return crudService.getStudent(id).get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable int id){
+        crudService.deleteStudent(id);
+    }
+
+    @PutMapping("/update")
+    public Student UpdateStudent(@RequestBody Student s){
+        return crudService.updateStudent(s).get();
+    }
+
+
+}
---
+++ CRUD/src/main/java/com/example/CRUD/model/Student.java
@@ -0,0 +1,50 @@
+package com.example.CRUD.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String location;
+
+    public Student() {
+    }
+
+    public Student( String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
