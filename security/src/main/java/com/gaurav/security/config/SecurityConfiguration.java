1c35dbe77adf7f3530bf74e07dcfe175e44e010d
Gaurb <89831065+Gaurb@users.noreply.github.com>
2025-03-30 20:02:43 +0530

    implemented JWT Authentication

---
+++ security/src/main/java/com/gaurav/security/config/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.gaurav.security.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                // Disable CSRF (not needed for stateless JWT)
+                .csrf(csrf -> csrf.disable())
+
+                // Configure endpoint authorization
+                .authorizeHttpRequests(auth -> auth
+                        // Public endpoints
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+
+                        // Role-based endpoints
+                        .requestMatchers("/auth/user/**").hasAuthority("ROLE_USER")
+                        .requestMatchers("/auth/admin/**").hasAuthority("ROLE_ADMIN")
+
+                        // All other endpoints require authentication
+                        .anyRequest().authenticated()
+                )
+
+                // Stateless session (required for JWT)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // Set custom authentication provider
+                .authenticationProvider(authenticationProvider)
+
+                // Add JWT filter before Spring Security's default filter
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
---
+++ security/src/main/java/com/gaurav/security/user/Role.java
@@ -0,0 +1,8 @@
+package com.gaurav.security.user;
+
+import jakarta.persistence.Enumerated;
+
+public enum Role {
+    USER,
+    ADMIN
+}
